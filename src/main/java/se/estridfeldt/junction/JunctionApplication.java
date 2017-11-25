22f25b19d61742fc8f16080ede16a3247b3bb3c3
emst <emil.stridfeldt@gmail.com>
2017-11-25 15:39:30 +0100

    Spring skeleton.

---
+++ src/main/java/se/estridfeldt/junction/JunctionApplication.java
@@ -0,0 +1,12 @@
+package se.estridfeldt.junction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunctionApplication.class, args);
+	}
+}
